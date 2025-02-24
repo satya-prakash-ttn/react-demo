44cc4285aac592cef48a74a8501ebd9e0e968999
Satya Prakash <satya.prakash@tothenew.com>
2025-02-24 08:59:33 +0530

    Added redirection

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+"use client"; 
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const NotFound = () => {
+    const router = useRouter();
+    const [countdown, setCountdown] = useState(5);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCountdown(prev => prev - 1);
+        }, 1000);
+
+        const redirectTimer = setTimeout(() => {
+            router.push("/");
+        }, 5000);
+
+        return () => {
+            clearInterval(timer);
+            clearTimeout(redirectTimer);
+        };
+    }, [router]);
+
     return (
-        <div className='container'>
-           <h1 className='text-4xl mb-8 mt-8'>Custom 404 Page</h1>
+        <div className="container text-center">
+            <h1 className="text-4xl mb-4 mt-8">404 - Page Not Found</h1>
+            <p className="text-lg">Redirecting to the homepage in {countdown} seconds...</p>
         </div>
     );
-}
+};
 
 export default NotFound;
